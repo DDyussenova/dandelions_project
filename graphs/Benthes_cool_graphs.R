459ef54a0deaa8afb45bd856f8e3bd533b7bfa83
Benthe van Kessel <benthe.vankessel@gmail.com>
2024-01-23 16:30:36 +0100

    BMI graph with interval

--- graphs/Benthes_cool_graphs.R
+++ graphs/Benthes_cool_graphs.R
@@ -6,14 +6,39 @@ graph1 <- read_csv("huge_csv.csv") |>
   mutate(BMI = (weight/1000)/(height^2)) |>
   mutate(weight_kg = weight/1000)
 
+BMI_filtered <- graph1 |> 
+  filter(!is.na(BMI)) |> 
+  mutate(BMI_bin = round((BMI + 1) / 2) * 2 - 1) |>
+  group_by(BMI_bin) |>
+  summarise(average_money = mean(prize_money),
+            sd_money = sd(prize_money),
+            n = n()) |>
+  #mutate(margin_of_error = qt(0.975, df = n - 1) * (sd_money / sqrt(n)))
+  mutate(
+    se_money = sd_money / sqrt(n),
+    lower_money = average_money - se_money,
+    upper_money = average_money + se_money
+  )
+
+ggplot(data = BMI_filtered) +
+  aes(x = BMI_bin, y = average_money) +
+  theme_light() +
+  scale_x_continuous(breaks = seq(15, 35, by = 2)) +
+  stat_summary_bin(fun = "mean", geom = "col", binwidth = 2) +
+  labs(y = "Average Prize Money (usd)",
+       x = "BMI") +
+  geom_pointrange(aes(ymin = lower_money, ymax = upper_money, na.rm = NULL)) +
+  geom_text(aes(label = n),
+            y = 5.05e+06)
+
 ggplot(data = graph1) +
   aes(x = height, y = prize_money) +
   scale_y_log10() +
   theme_light() +
   geom_point(position = position_jitter(0.008)) +
   geom_smooth(method = "lm") +
-  labs(x = "Height",
-       y = "Prize Money")
+  labs(x = "Height (m)",
+       y = "Prize Money (usd)")
 
 ggplot(data = graph1) +
   aes(x = weight_kg, y = prize_money) +
@@ -21,39 +46,31 @@ ggplot(data = graph1) +
   theme_light() +
   geom_point(position = position_jitter(0.01)) +
   geom_smooth(method = "lm") +
-  labs(x = "Weight",
-       y = "Prize Money")
+  labs(x = "Weight (kg)",
+       y = "Prize Money (usd)")
 
 ggplot(data = graph1) +
   aes(x = height, y = prize_money) +
   theme_light() +
   scale_x_continuous(breaks = seq(1.5, 2.1, by = 0.05)) +
   stat_summary_bin(fun = "mean", geom = "col", binwidth = 0.05) +
-  labs(y = "Average Prize Money",
-       x = "Height")
+  labs(y = "Average Prize Money (usd)",
+       x = "Height (m)")
 
 ggplot(data = graph1) +
   aes(x = weight_kg, y = prize_money) +
   theme_light() +
   scale_x_continuous(breaks = seq(45, 105, by = 5)) +
   stat_summary_bin(fun = "mean", geom = "col", binwidth = 5) +
-  labs(y = "Average Prize Money",
-       x = "Weight")
-
-ggplot(data = graph1) +
-  aes(x = BMI, y = prize_money) +
-  theme_light() +
-  scale_x_continuous(breaks = seq(15, 35, by = 2)) +
-  stat_summary_bin(fun = "mean", geom = "col", binwidth = 2) +
-  labs(y = "Average Prize Money",
-       x = "BMI")
+  labs(y = "Average Prize Money (usd)",
+       x = "Weight (kg)")
 
 ggplot(data = graph1) +
   aes(x = year, y = prize_money) +
   theme_light() +
   scale_x_continuous(breaks = seq(1930, 2000, by = 10)) +
   stat_summary_bin(fun = "mean", geom = "col", binwidth = 5) +
-  labs(y = "Average Prize Money",
+  labs(y = "Average Prize Money (usd)",
        x = "Birth Year")
 
 ggplot(data = graph1) +
@@ -61,5 +78,5 @@ ggplot(data = graph1) +
   theme_light() +
   scale_x_continuous(breaks = seq(1, 12, by = 1)) +
   stat_summary_bin(fun = "mean", geom = "col", binwidth = 1) +
-  labs(y = "Average Prize Money",
+  labs(y = "Average Prize Money (usd)",
        x = "Birth Month")
