f086f1428f66b69d4622ee5331ff99d71e165a06
Benthe van Kessel <benthe.vankessel@gmail.com>
2024-01-24 10:45:57 +0100

    Adding y-axis names

--- graphs/Graphs_1_2_3_7.R
+++ graphs/Graphs_1_2_3_7.R
@@ -6,6 +6,7 @@ graph1 <- read_csv("huge_csv.csv") |>
   mutate(BMI = (weight/1000)/(height^2)) |>
   mutate(weight_kg = weight/1000)
 
+#Graph BMI/Money
 BMI_filtered <- graph1 |> 
   filter(!is.na(BMI)) |> 
   mutate(BMI_bin = round((BMI + 1) / 2) * 2 - 1) |>
@@ -13,24 +14,24 @@ BMI_filtered <- graph1 |>
   summarise(average_money = mean(prize_money),
             sd_money = sd(prize_money),
             n = n()) |>
-  #mutate(margin_of_error = qt(0.975, df = n - 1) * (sd_money / sqrt(n)))
   mutate(
     se_money = sd_money / sqrt(n),
     lower_money = average_money - se_money,
-    upper_money = average_money + se_money
-  )
+    upper_money = average_money + se_money)
 
 ggplot(data = BMI_filtered) +
   aes(x = BMI_bin, y = average_money) +
   theme_light() +
   scale_x_continuous(breaks = seq(15, 35, by = 2)) +
+  scale_y_continuous(labels = scales::number_format(scale = 1e-6, accuracy = 1, suffix = "")) + 
   stat_summary_bin(fun = "mean", geom = "col", binwidth = 2) +
-  labs(y = "Average Prize Money (usd)",
+  labs(y = "Average Prize Money (in millions USD)",
        x = "BMI") +
   geom_pointrange(aes(ymin = lower_money, ymax = upper_money, na.rm = NULL)) +
   geom_text(aes(label = n),
             y = 5.05e+06)
 
+#Graph Height/Money 
 ggplot(data = graph1) +
   aes(x = height, y = prize_money) +
   scale_y_log10() +
