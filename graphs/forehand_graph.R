67f86cd65b2bf359b5acedb1c01eacfb882a9808
Dzhamilya <d.dyussenova@gmail.com>
2024-01-24 10:34:14 +0100

    forhand_graph finished

--- graphs/forehand_graph.R
+++ graphs/forehand_graph.R
@@ -12,7 +12,22 @@ forehand_filtered <- tennis_players_data |>
   mutate(
     se_money = sd_money / sqrt(n),
     lower_money = average_money - se_money,
-    upper_money = average_money + se_money) |>
+    upper_money = average_money + se_money) |> 
   print()
-  print()
+
+ggplot(data = forehand_filtered) +
+  aes(x = forehand, y = average_money) +
+  theme_light()+
+  geom_col()+
+  geom_pointrange(aes(ymin = lower_money, ymax = upper_money))+
+  annotate("text",                        
+           x = NA, y = 1900000, 
+           label = "n = 3362", 
+           family = 'sans')+
+  annotate("text", 
+           x = 'double', y = 2100000, 
+           label = "n = 55", 
+           family = 'sans') + 
+  labs(y = "Average Career Prize Money (USD)",
+       x = "Forehand")
   
